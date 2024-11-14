8b46a040275f7ebfce0c612f0b0974f0ec430d35
emvmart23 <max.martinez@tecsup.edu.pe>
2024-11-14 14:45:19 -0500

    🔥 Removing code of projects

--- src/pages/Projects/data/dataProjects.ts
+++ src/pages/Projects/data/dataProjects.ts
@@ -15,70 +15,11 @@ export const data = [
         name: "Jwt",
       },
     ],
-    description: "Esta API está diseñada para facilitar la gestión de pedidos y la asistencia de usuarios en tiempo real. Permite que múltiples usuarios realicen solicitudes simultáneamente, garantizando un flujo de trabajo eficiente y organizado. Además, la API ofrece la funcionalidad de aperturar una caja diaria, registrando la fecha y almacenándola como referencia para futuras operaciones. Esto permite filtrar y consultar las tablas relacionadas por dicha fecha, mejorando así la organización y el acceso a la información. Con esta API, los locales pueden optimizar la gestión de pedidos y la atención al cliente, asegurando un servicio ágil y efectivo.",
+    description:
+      "Esta API está diseñada para facilitar la gestión de pedidos y la asistencia de usuarios en tiempo real. Permite que múltiples usuarios realicen solicitudes simultáneamente, garantizando un flujo de trabajo eficiente y organizado. Además, la API ofrece la funcionalidad de aperturar una caja diaria, registrando la fecha y almacenándola como referencia para futuras operaciones. Esto permite filtrar y consultar las tablas relacionadas por dicha fecha, mejorando así la organización y el acceso a la información. Con esta API, los locales pueden optimizar la gestión de pedidos y la atención al cliente, asegurando un servicio ágil y efectivo.",
     deploy: null,
     github: "https://github.com/emvmart23/Backend-club",
     image: "./backendclub.svg",
-  },
-  {
-    name: "Todo App",
-    lenguages: [
-      {
-        logo: "./logoTypescript.webp",
-        name: "Typescript",
-      },
-      {
-        logo: "./logoNode.webp",
-        name: "NodeJS",
-      },
-      {
-        logo: "./logoReact.webp",
-        name: "ReactJS",
-      },
-      {
-        logo: "./logoNext.webp",
-        name: "NextJS",
-      },
-      {
-        logo: "./logoRedux.webp",
-        name: "Redux",
-      },
-    ],
-    description:
-      "Con Todo App, los usuarios pueden crear nuevas tareas, marcarlas como completadas y eliminarlas según sea necesario. La integración de Redux Toolkit permite una gestión eficiente del estado de la aplicación, garantizando una experiencia de usuario sin problemas y consistente. Además, todas las tareas se almacenan localmente en el LocalStorage del navegador, lo que proporciona persistencia de datos entre sesiones.",
-    deploy: "https://todo-redux-toolkit-sandy.vercel.app/",
-    github: "https://github.com/emvmart23/Todo-redux-toolkit",
-    image: "./todo.svg",
-  },
-  {
-    name: "Cerrajeria Apertura porte",
-    lenguages: [
-      {
-        logo: "./logoTypescript.webp",
-        name: "Typescript",
-      },
-      {
-        logo: "./logoNode.webp",
-        name: "NodeJS",
-      },
-      {
-        logo: "./logoReact.webp",
-        name: "ReactJS",
-      },
-      {
-        logo: "./logoVite.webp",
-        name: "ViteJS",
-      },
-      {
-        logo: "./logoTailwind.webp",
-        name: "Tailwind",
-      },
-    ],
-    description:
-      "Apertura Porte es una aplicación web desarrollada utilizando React.js, TypeScript y Tailwind CSS para mostrar servicios de cerrajería de calidad a nuestros clientes. Nuestro objetivo es proporcionar una experiencia en línea fácil y segura para quienes necesitan servicios de cerrajería, desde la apertura de puertas hasta la instalación de cerraduras.",
-    deploy: "https://apertura-porte.vercel.app/",
-    github: "https://github.com/HAms25/Cerrajeria-Martinez",
-    image: "./cerrajeria.svg",
   },
   {
     name: "Api Autenticación",
@@ -123,6 +64,66 @@ export const data = [
     deploy: null,
     github: "https://github.com/emvmart23/UsersType",
     image: "./usertype.svg",
+  },
+  {
+    name: "Cerrajeria Apertura porte",
+    lenguages: [
+      {
+        logo: "./logoTypescript.webp",
+        name: "Typescript",
+      },
+      {
+        logo: "./logoNode.webp",
+        name: "NodeJS",
+      },
+      {
+        logo: "./logoReact.webp",
+        name: "ReactJS",
+      },
+      {
+        logo: "./logoVite.webp",
+        name: "ViteJS",
+      },
+      {
+        logo: "./logoTailwind.webp",
+        name: "Tailwind",
+      },
+    ],
+    description:
+      "Apertura Porte es una aplicación web desarrollada utilizando React.js, TypeScript y Tailwind CSS para mostrar servicios de cerrajería de calidad a nuestros clientes. Nuestro objetivo es proporcionar una experiencia en línea fácil y segura para quienes necesitan servicios de cerrajería, desde la apertura de puertas hasta la instalación de cerraduras.",
+    deploy: "https://apertura-porte.vercel.app/",
+    github: "https://github.com/HAms25/Cerrajeria-Martinez",
+    image: "./cerrajeria.svg",
+  },
+  {
+    name: "Todo App",
+    lenguages: [
+      {
+        logo: "./logoTypescript.webp",
+        name: "Typescript",
+      },
+      {
+        logo: "./logoNode.webp",
+        name: "NodeJS",
+      },
+      {
+        logo: "./logoReact.webp",
+        name: "ReactJS",
+      },
+      {
+        logo: "./logoNext.webp",
+        name: "NextJS",
+      },
+      {
+        logo: "./logoRedux.webp",
+        name: "Redux",
+      },
+    ],
+    description:
+      "Con Todo App, los usuarios pueden crear nuevas tareas, marcarlas como completadas y eliminarlas según sea necesario. La integración de Redux Toolkit permite una gestión eficiente del estado de la aplicación, garantizando una experiencia de usuario sin problemas y consistente. Además, todas las tareas se almacenan localmente en el LocalStorage del navegador, lo que proporciona persistencia de datos entre sesiones.",
+    deploy: "https://todo-redux-toolkit-sandy.vercel.app/",
+    github: "https://github.com/emvmart23/Todo-redux-toolkit",
+    image: "./todo.svg",
   },
   {
     name: "App StarWars",
